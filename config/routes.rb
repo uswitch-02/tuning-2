fdf579a0dfcbee5c520e0f45cd2c6619b42a7bb1
uswitch-02 <chunxidubian64@gmail.com>
2023-06-08 05:15:45 +0000

    [create]routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,37 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  # devise_for :customers
+
+ namespace :admin do
+    get '/' => 'homes#top'
+    resources :customers,   only: %i(index show edit update) do
+    end
+  end
+  
+  scope module: :public do
+    root :to                             =>  'homes#top'
+    get    '/about'                      =>  'homes#about'
+    get    'customers/withdraw_confirm'  =>  'customers#withdraw_confirm'
+    patch  'customers/withdraw'          =>  'customers#withdraw'
+    get    '/customers'                  =>  'customers#show'
+    get    'customers/information/edit', to: 'customers#edit',   as: 'edit_customer'
+    patch  'customers/information',      to: 'customers#update', as: 'update_customer'
+    get    'customer/search'             =>  'customer#search'
+  end
+
+  resources :diarys,       only: %i(index show create edit update destroy) do
+    resource :favorites, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
+  end
+
+
+  resources :customers,  only: %i(show edit update) do
+    resource :relationships, only: %i(create destroy)
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+  resources :sentiments,      only: %i(create destroy)
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
