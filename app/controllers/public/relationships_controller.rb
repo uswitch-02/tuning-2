5cf6e8cc1d3046f7bab29982bcf6356bba8b79ba
uswitch-02 <chunxidubian64@gmail.com>
2023-06-08 09:47:00 +0000

    [update]diary-crud

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,2 +1,24 @@
 class Public::RelationshipsController < ApplicationController
+
+    def create
+      customer = Customer.find(params[:customer_id])
+      current_customer.follow(customer)
+      redirect_to request.referer
+    end
+
+    def destroy
+      customer = Customer.find(params[:customer_id])
+      current_customer.unfollow(customer)
+      redirect_to request.referer
+    end
+
+    def followings
+      customer = Customer.find(params[:customer_id])
+      @customer = customer.followings
+    end
+
+    def followers
+      customer = Customer.find(params[:user_id])
+      @customer = customer.followers
+    end
 end
