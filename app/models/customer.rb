e4a763e575ccc1c7e07f484fdc904ce33322c9a3
uswitch-02 <chunxidubian64@gmail.com>
2023-06-08 22:59:13 +0000

    [install]active_storage

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,11 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   enum is_deleted: { exist: false, withdraw: true }
   enum is_published: { disclose: true, non_disclosure: false }
   enum is_locked: { unlook: false, look: true }
-  
+
   has_many :diary, dependent: :destroy
   has_many :report, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -15,7 +15,7 @@ class Customer < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
@@ -23,12 +23,13 @@ class Customer < ApplicationRecord
   validates :pen_name, presence: true
   validates :email, presence: true
   validates :encrypted_password, presence: true,length: { minimum: 6 }
-  
+
   has_one_attached :profile_image
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
 
   def follow(customer)
     relationships.create(followed_id: customer.id)
@@ -41,7 +42,7 @@ class Customer < ApplicationRecord
   def following?(customer)
     followings.include?(customer)
   end
-  
+
   # ユーザ検索機能
   def self.search_for(content, method)
     if method == 'perfect'
