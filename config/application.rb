938053ea1e6dbd5e186f72747a1ea82848c9a77c
uswitch-02 <chunxidubian64@gmail.com>
2023-07-01 13:21:28 +0000

    [update]

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Tuning2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.paths.add 'lib', eager_load: true # 追加
     # Configuration for the application, engines, and railties goes here.
     #
